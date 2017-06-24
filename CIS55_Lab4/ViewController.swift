02f9ed3d584ce2a430ff6b304fb878749f7716ad
Sergey Chertilin <sergey_h@list.ru>
2017-06-24 08:53:45 -0700

    First run

--- CIS55_Lab4/ViewController.swift
+++ CIS55_Lab4/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var startLocation: UITextField!
+    @IBOutlet weak var destination: UITextField!
+    
+    var myLockMgr = CLLocationManager()
+    var myGeoCoder = CLGeocoder()
+    var showPlacemak : CLPlacemark?
+    
+    var toAddr : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLockMgr.requestWhenInUseAuthorization()
+        let status = CLLocationManager.authorizationStatus()
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            self.myMap.showsUserLocation = true
+        }
+        myMap.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func locate(_ sender: Any) {
+    }
 
+    @IBAction func directions(_ sender: Any) {
+    }
 }
 
